382b6b3a11b40d836bba833e837efed96d632d47
Myagoo <millagou.benjamin@gmail.com>
2014-12-09 01:03:24 +0100

    added automatic resolve of json files

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
     }]
   },
   resolve: {
-    extensions: ['', '.js', '.jsx'],
+    extensions: ['', '.js', '.jsx', '.json'],
     modulesDirectories: ['node_modules', 'bower_components', 'src']
   }
 };
